01b081be24229f3f3fce0a09b78b553322a16d22
muditha <muditha.kumara@centria.fi>
2025-07-18 00:36:15 +0300

    my work

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,12 @@ const gulp = require("gulp"),
       rimraf = require("rimraf"),
       concat = require("gulp-concat"),
       cleanCSS = require("gulp-clean-css"),
-      uglify = require("gulp-uglify");
+      uglify = require("gulp-uglify"),
+      gulpSass = require("gulp-sass"),
+      sass = require("sass");
+
+// Configure gulp-sass to use Dart Sass
+gulpSass.compiler = sass;
 
 const paths = {
   webroot: "./Tailspin.SpaceGame.Web/wwwroot/"
@@ -15,12 +20,19 @@ paths.js = paths.webroot + "js/**/*.js";
 paths.minJs = paths.webroot + "js/**/*.min.js";
 paths.css = paths.webroot + "css/**/*.css";
 paths.minCss = paths.webroot + "css/**/*.min.css";
+paths.scss = paths.webroot + "css/**/*.scss";
 paths.concatJsDest = paths.webroot + "js/site.min.js";
 paths.concatCssDest = paths.webroot + "css/site.min.css";
 
 gulp.task("clean:js", done => rimraf(paths.concatJsDest, done));
 gulp.task("clean:css", done => rimraf(paths.concatCssDest, done));
 gulp.task("clean", gulp.series(["clean:js", "clean:css"]));
+
+gulp.task("compile:scss", () => {
+  return gulp.src(paths.scss)
+    .pipe(gulpSass().on('error', gulpSass.logError))
+    .pipe(gulp.dest(paths.webroot + "css"));
+});
 
 gulp.task("min:js", () => {
   return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
@@ -29,12 +41,12 @@ gulp.task("min:js", () => {
     .pipe(gulp.dest("."));
 });
 
-gulp.task("min:css", () => {
+gulp.task("min:css", gulp.series(["compile:scss"], () => {
   return gulp.src([paths.css, "!" + paths.minCss])
     .pipe(concat(paths.concatCssDest))
     .pipe(cleanCSS())
     .pipe(gulp.dest("."));
-});
+}));
 
 gulp.task("min", gulp.series(["min:js", "min:css"]));
 
